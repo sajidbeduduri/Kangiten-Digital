99f1b44ec03b66ead91ef11fee7ff62a042dc8da
sajidbeduduri <127605937+sajidbeduduri@users.noreply.github.com>
2025-09-14 13:55:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Smooth scroll for nav links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
+  anchor.addEventListener("click", function(e){
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"});
   });
+});
+
+// Scroll to quote form button
+document.querySelector('.btn-quote').addEventListener("click", function(e){
+  e.preventDefault();
+  document.querySelector('#quote-form').scrollIntoView({behavior:"smooth"});
 });
